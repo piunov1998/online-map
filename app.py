8ae094446e026584746d4794f2bf35d5baadce8e
Anton <piunov.doc@gmail.com>
2022-10-26 01:56:11 +0300

    [add]: накрутил flask

---
+++ app.py
@@ -0,0 +1,12 @@
+import flask
+
+
+app = flask.Flask(__name__)
+
+
+@app.route('/map')
+def main():
+    return flask.render_template('main.html')
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', 5555)
